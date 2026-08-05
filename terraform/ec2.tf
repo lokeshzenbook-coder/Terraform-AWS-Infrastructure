ff515410ba224eaace51af837170e505b4b41469
lokeshzenbook-coder <lokeshzenbook@gmail.com>
2026-08-05 07:39:42 +0000

    ec2 file changed

--- terraform/ec2.tf
+++ terraform/ec2.tf
@@ -1,27 +1,12 @@
-data "aws_ami" "amazon_linux" {
-  most_recent = true
-  owners      = ["amazon"]
-
-  filter {
-    name   = "name"
-    values = ["al2023-ami-*-x86_64"]
-  }
-
-  filter {
-    name   = "virtualization-type"
-    values = ["hvm"]
-  }
-}
-
 resource "aws_instance" "app" {
   ami                    = data.aws_ami.amazon_linux.id
   instance_type          = var.instance_type
   subnet_id              = aws_subnet.public[0].id
   vpc_security_group_ids = [aws_security_group.web.id]
   iam_instance_profile   = aws_iam_instance_profile.ec2_profile.name
-  key_name                = var.key_pair_name
-  monitoring              = true   # CKV_AWS_126
-  ebs_optimized           = true   # CKV_AWS_135
+  key_name               = var.key_pair_name
+  monitoring              = true # CKV_AWS_126
+  ebs_optimized           = true # CKV_AWS_135
 
   metadata_options {
     http_tokens   = "required"
